8fcfefa90784bfef9f5d918195e72c5f95f7a8fc
Cesare Laurenziello <claurenziello@sogei.it>
2022-03-17 16:12:20 +0000

    Remove dist

--- dist/bsons-app/assets/resources/js/uikit.js
+++
@@ -1,6 +0,0 @@
-/*
- * Questo file serve solo se si utilizza lo UI Kit come modulo JavaScript
- */
-require('./bootstrap.bundle.min.js');
-require('./bootstrap-datepicker.min.js');
-require('./bootstrap-datepicker.it.min.js');
